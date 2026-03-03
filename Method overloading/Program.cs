4cf7169b075db5664cc03c46ca048a7261be4910
Naresh <Naresh@DESKTOP-I6DSPIH>
2026-03-03 11:16:35 +0530

    Add project files.

---
+++ Method overloading/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Add(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+    static void Main()
+    {
+        Calculator obj = new Calculator();
+        Console.WriteLine(obj.Add(5, 10));       
+        Console.WriteLine(obj.Add(5, 10, 15));   
+    }
+}
